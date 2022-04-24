0d97e8fef11f0f3e29c812f4d326788d4f97c0ac
Bawskiend <Frayzerka@gmail.com>
2022-04-24 22:42:37 +0500

    24.04 homepc

---
+++ js/script.js
@@ -0,0 +1,8 @@
+new Swiper('.swiper', {
+    loop: 'true',
+    autoHeight:'true',
+    direction: 'vertical',
+    navigation: {
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev'},
+    });
