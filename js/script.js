2c2fce7b709da3251b744f52344a35a249150970
Румянцев Артём Игоревич <A.Rumyantsev@UKZ.LOCAL>
2022-04-25 17:42:26 +0500

    25.04 workpc

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 new Swiper('.swiper', {
     loop: 'true',
     autoHeight:'true',
-    direction: 'vertical',
+    effect:'flip',
+    flipEffect:{
+        slideShadows: true,
+        
+    },
     navigation: {
         nextEl:'.swiper-button-next',
         prevEl:'.swiper-button-prev'},
